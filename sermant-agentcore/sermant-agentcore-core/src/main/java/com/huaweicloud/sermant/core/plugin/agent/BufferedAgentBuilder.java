5381867464b604bd652e98175446dff997edb422
luanwenfei <luanwenfei@outlook.com>
2023-04-11 18:28:21 +0800

    BootstrapTemplate合并

--- sermant-agentcore/sermant-agentcore-core/src/main/java/com/huaweicloud/sermant/core/plugin/agent/BufferedAgentBuilder.java
+++ sermant-agentcore/sermant-agentcore-core/src/main/java/com/huaweicloud/sermant/core/plugin/agent/BufferedAgentBuilder.java
@@ -28,6 +28,7 @@ import com.huaweicloud.sermant.core.utils.FileUtils;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.agent.builder.AgentBuilder.Default;
 import net.bytebuddy.agent.builder.ResettableClassFileTransformer;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.description.type.TypeList.Generic;
@@ -274,11 +275,10 @@ public class BufferedAgentBuilder {
      * @return 安装结果，可重置的转换器，若无类元信息改动，调用其reset方法即可重置
      */
     public ResettableClassFileTransformer install(Instrumentation instrumentation) {
-        AgentBuilder builder = new AgentBuilder.Default(new ByteBuddy());
+        AgentBuilder builder = new Default().disableClassFormatChanges();
         for (BuilderAction action : actions) {
             builder = action.process(builder);
         }
-        builder.disableClassFormatChanges();
         return builder.installOn(instrumentation);
     }
 
